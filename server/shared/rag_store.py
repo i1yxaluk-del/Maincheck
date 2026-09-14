ab121a67e26c42e0fc2f77bb7a5f35ef4bb7378c
i1yxaluk-del <i1yxaluk@gmail.com>
2026-09-14 16:57:23 +0300

    fix(rag): prioritize exact terms with hybrid FTS retrieval

--- server/shared/rag_store.py
+++ server/shared/rag_store.py
@@ -5,10 +5,10 @@ from .rag_chunking import safe_chunk_text  # noqa: E402
 from .rag_office_extract import read_office_legacy  # noqa: E402
 from . import rag_store_v2 as _rag_store_v2  # noqa: E402
 
-_rag_store_v2.garant_cleanup.chunk_text = safe_chunk_text
-_rag_store_v2.garant_cleanup._EXTRACTORS['.doc'] = read_office_legacy
-_rag_store_v2.garant_cleanup._EXTRACTORS['.rtf'] = read_office_legacy
+_rag_store_v2.garant_cleanup.chunk_text=safe_chunk_text
+_rag_store_v2.garant_cleanup._EXTRACTORS['.doc']=read_office_legacy
+_rag_store_v2.garant_cleanup._EXTRACTORS['.rtf']=read_office_legacy
 _rag_store_v2.EXTS.update({'.doc','.rtf'})
 
-from .rag_incremental import IncrementalRagStore  # noqa: E402
-RagStore = IncrementalRagStore
+from .rag_hybrid import HybridRagStore  # noqa: E402
+RagStore=HybridRagStore
